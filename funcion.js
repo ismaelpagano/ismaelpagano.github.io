18246d3a217fe8c3f65cc57a4ee1ec898239fda7
ismaelpagano <43893186+ismaelpagano@users.noreply.github.com>
2021-05-09 01:21:37 -0300

    Update funcion.js

--- funcion.js
+++ funcion.js
@@ -1,1 +1,25 @@
-alert("Hola Pagano")
+var texto = "Querés aprender programación?";
+
+function escribir(texto1){
+    var texto2 = '';
+
+    function loop(tex1, tex2, i){
+            var char = tex1[i];
+            tex2 += char;
+            i++;
+            texto2 = tex2;
+            document.getElementById("texto").innerHTML = texto2;
+            if(i<tex1.length)
+                this.setTimeout(function(){
+                    loop(tex1, tex2, i);
+            }, 100);
+        console.log("hola");
+    }
+    
+    loop(texto, texto2, 0)
+
+    console.log(texto2)
+}
+
+
+escribir(texto);
